14da98918790a1ed7d67b3fd896fa7e88f412093
Muhammad Hanif <hm6280804@gmail.com>
2025-09-01 19:46:40 +0500

    commit changes to db

--- config.php
+++ config.php
@@ -1,9 +1,13 @@
 <?php
 // config.php - Fill in your MySQL credentials
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'php_auth_app');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', 'enterprise-postgresql.postgres.database.azure.com');
+define('DB_NAME', 'testing');
+define('DB_USER', 'postgres');
+define('AZURE_DB_PORT', 5432);
+
+define('DB_PASS', 'fYUBVcZ01QeNOfkl');
 
 // Optional: app name
 define('APP_NAME', 'Simple PHP Auth');
+
+
--- db.php
+++ db.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/config.php';
 function db() : PDO {
     static $pdo = null;
     if ($pdo === null) {
-        $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4';
+        $dsn = 'pgsql:host=' . DB_HOST . ';port=' . AZURE_DB_PORT . ';dbname=' . DB_NAME . ';charset=utf8mb4';
         $pdo = new PDO($dsn, DB_USER, DB_PASS, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
