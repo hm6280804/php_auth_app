95eb7c9624f571bb27916f80102c6df87ea0a315
Muhammad Hanif <hm6280804@gmail.com>
2025-09-02 12:33:07 +0500

    changes in the register.php

--- register.php
+++ register.php
@@ -47,7 +47,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 exit;
             }
         } catch (PDOException $e) {
-            $errors[] = 'Registration failed.';
+            $errors[] = 'Registration failed.' . $e->getMessage();
         }
     }
 }
