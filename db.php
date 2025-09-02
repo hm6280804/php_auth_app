41b24397b11e2f24625b4f78b6647f444f173ed4
Muhammad Hanif <hm6280804@gmail.com>
2025-09-02 12:26:25 +0500

    changes made to config

--- db.php
+++ db.php
@@ -4,8 +4,9 @@ require_once __DIR__ . '/config.php';
 
 function db() : PDO {
     static $pdo = null;
+    $port = AZURE_DB_PORT ?? 5432;
     if ($pdo === null) {
-        $dsn = 'pgsql:host=' . DB_HOST . ';port=' . AZURE_DB_PORT . ';dbname=' . DB_NAME . ';charset=utf8mb4';
+        $dsn = 'pgsql:host=' . DB_HOST . ';port=' . $port . ';dbname=' . DB_NAME;
         $pdo = new PDO($dsn, DB_USER, DB_PASS, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
